50c726a090f223525b9eb774b2e7724dc93c992c
EBFEh <EBFEh>
2015-03-08 01:47:07 -0500

    Changes to the quest file, scene, and a new ScrollManager for Quest-related UI

---
+++ Assets/ScrollManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Daggerfall.Game { 
+    public class ScrollManager : MonoBehaviour {
+        public Canvas smallScroll;
+        public Text smallScrollText;
+        public UIManager uiManager;
+
+        // Use this for initialization
+        void Start () {
+        
+        }
+        
+        // Update is called once per frame
+        void Update () {
+            if (Input.GetMouseButtonDown(0)) {
+                this.smallScroll.enabled = false;
+                uiManager.SendMessage("uiElementClosed");
+            }
+        }
+
+        void OnGUI() {
+        }
+
+        public void displayScroll(string output) {
+            // TODO: Create a large scroll and figure out when to use it, even for localized text!
+            smallScrollText.text = output;
+            smallScroll.enabled = true;
+            uiManager.SendMessage("uiElementOpened");
+        }
+    }
+}

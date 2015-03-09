add045ad7862e28ba5a287dc43d826bd59157601
Dan <dan@inkstripe.com>
2015-03-08 21:53:00 -0400

    Checking in project files

--- Assets/ScrollManager.cs
+++ Assets/ScrollManager.cs
@@ -1,11 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 namespace Daggerfall.Game { 
-    public class ScrollManager : MonoBehaviour {
-        public Canvas smallScroll;
-        public Text smallScrollText;
+    public class ScrollManager : PrefabStateMachine {
         public UIManager uiManager;
 
         // Use this for initialization
@@ -15,10 +14,11 @@ namespace Daggerfall.Game {
         
         // Update is called once per frame
         void Update () {
-            if (Input.GetMouseButtonDown(0)) {
-                this.smallScroll.enabled = false;
+/*            if (Input.GetMouseButtonDown(0)) {
+                smallScroll.SetActive(false);
                 uiManager.SendMessage("uiElementClosed");
-            }
+            }*/
+            base.Update();
         }
 
         void OnGUI() {
@@ -26,9 +26,18 @@ namespace Daggerfall.Game {
 
         public void displayScroll(string output) {
             // TODO: Create a large scroll and figure out when to use it, even for localized text!
-            smallScrollText.text = output;
-            smallScroll.enabled = true;
+            changeState(PrefabState.UI_SCROLL_SMALL);
+            sendMessageToPrefab(new KeyValuePair<string,object>("displayText", output));
             uiManager.SendMessage("uiElementOpened");
+
+/*            smallScrollText.text = output;
+            smallScroll.SetActive(true);
+            uiManager.SendMessage("uiElementOpened");*/
+        }
+
+        public void closeScroll() { 
+            changeState(PrefabState.UI_OFF);
+            uiManager.SendMessage("uiElementClosed");
         }
     }
 }
